b124b9b114395614df39a98d187367d994b00e22
Turdibek <turdibekdev05@gmail.com>
2022-05-07 22:25:13 +0500

    Filterlar qo'shildi

--- bot/filters/__init__.py
+++ bot/filters/__init__.py
@@ -1,9 +1,12 @@
 from aiogram import Dispatcher
 
 from loader import dp
-# from .is_admin import AdminFilter
+from .is_admin import IsAdmin
+from .is_private import IsPrivate
+from .is_group import IsGroup
 
 
 if __name__ == "filters":
-    #dp.filters_factory.bind(is_admin)
-    pass
+    dp.filters_factory.bind(IsAdmin)
+    dp.filters_factory.bind(IsPrivate)
+    dp.filters_factory.bind(IsGroup)
