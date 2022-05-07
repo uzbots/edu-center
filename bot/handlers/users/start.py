959cbfca62cb744e4876249d889dfe49ee5f4d04
Turdibek <turdibekdev05@gmail.com>
2022-05-07 22:34:04 +0500

    /start - buyrug'i

--- bot/handlers/users/start.py
+++ bot/handlers/users/start.py
@@ -2,8 +2,12 @@ from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 
 from loader import dp
+from filters import IsPrivate
 
 
-@dp.message_handler(CommandStart())
+@dp.message_handler(IsPrivate(), CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f"Salom, {message.from_user.full_name}!")
+    matn = f"<b>Assalom alaykum hurmatli {message.from_user.full_name}!</b>\n"
+    matn += "Sizni <b>EDU-Center</b> akademyasini rasmiy Telegram botida ko'rib turganimdan hursandman.\n"
+    matn += "Marhamat ro'yhatdan o'tish uchun /register buyrug'ini kiriting..."
+    await message.answer(text=matn)
